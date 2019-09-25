60b3bb32e0fd50589318ce6deecceda9a218b94c
mzapataj <mzapataj@uninorte.edu.co>
2019-09-25 16:16:04 -0500

    Login activy added

--- app/src/main/java/com/example/primerparcial/LoginActivity.java
+++ app/src/main/java/com/example/primerparcial/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.primerparcial;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.EventLog;
+import android.view.View;
+
+import androidx.annotation.MainThread;
 
 public class LoginActivity extends Activity {
 
@@ -11,5 +16,9 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.login_activity_layout);
     }
 
+    public void ToRegister(View view){
+        Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+        startActivity(intent);
+    }
 
 }
