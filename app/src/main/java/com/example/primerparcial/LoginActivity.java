c03ca1d3fb10a5fac0de3ae1261aaeae1abc8cd3
mzapataj <mzapataj@uninorte.edu.co>
2019-09-28 18:27:01 -0500

    Corrected login activity

--- app/src/main/java/com/example/primerparcial/LoginActivity.java
+++ app/src/main/java/com/example/primerparcial/LoginActivity.java
@@ -7,6 +7,7 @@ import android.util.EventLog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.MainThread;
 
@@ -16,22 +17,28 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_layout);
-        ((Button)findViewById(R.id.submit))
-                .setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent intent = new Intent(getApplicationContext(),
-                                MainActivity.class);
-                        intent.putExtra("user_name",
-                                ((EditText)findViewById(
-                                        R.id.username)).getText().toString());
-                        intent.putExtra("user_password",
-                                ((EditText)findViewById(
-                                        R.id.password)).getText().toString());
-                        startActivity(intent);
 
-                    }
-                });
+            ((Button) findViewById(R.id.submit))
+                    .setOnClickListener(new View.OnClickListener() {
+
+
+                        @Override
+                        public void onClick(View view) {
+                            try {
+                                Intent intent = new Intent(getApplicationContext(),
+                                        MainActivity.class);
+                                intent.putExtra("user_name",
+                                        ((EditText) findViewById(
+                                                R.id.email)).getText().toString());
+                                intent.putExtra("user_password",
+                                        ((EditText) findViewById(
+                                                R.id.password)).getText().toString());
+                                startActivity(intent);
+                            } catch(Exception error){
+                                Toast.makeText(LoginActivity.this,"Must put your email or/and password " ,Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
 
     }
 
